11ab24fe9d8b7a22ee9462716b2bf4f425386fc5
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-06-30 20:12:26 +0100

    --- Auto-Git Commit ---

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
 let canvas = document.getElementById("gamescreen");
 let ctx = canvas.getContext("2d");
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 ctx.clearRect(0, 0, 800, 600);
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
